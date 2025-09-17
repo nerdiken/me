2020bf39a9a1c308be099131d26225a3ffcaac89
nerdiken <mbosioken@gmail.com>
2025-09-17 20:17:57 +0200

    Set base path in `astro.config.ts`

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import UnoCSS from 'unocss/astro';
 export default defineConfig({
   output: "static",
   // used to generate images
+  base: '/',
   site: 'https://www.nerdiken.me/',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
